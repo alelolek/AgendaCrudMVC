a0a4754fc0f86df4f952c2f24e94e11172f6518f
alelolek <alelolek.dev@gmail.com>
2023-04-19 21:28:52 -0500

    Agregar archivos de proyecto.

---
+++ AgendaCrud/Models/AgendaContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaCrud.Models;
+
+public partial class AgendaContext : DbContext
+{
+    public AgendaContext()
+    {
+    }
+
+    public AgendaContext(DbContextOptions<AgendaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("server=DESKTOP-1TL06GG\\SQLEXPRESS; database= Agenda; integrated security=true; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Clientes__3214EC07F3BB8758");
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Empresa)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Numero)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ AgendaCrud/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgendaCrud.Models;
+
+public partial class Cliente
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Numero { get; set; }
+
+    public string? Empresa { get; set; }
+}
